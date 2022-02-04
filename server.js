d1f6010decf393ed95d897b251d090918ffd7455
msakac <msakac@foi.hr>
2022-02-04 12:21:09 +0100

    Created login page design

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ mongoose.connect('mongodb://localhost/tbprojekt', { useNewUrlParser: true, useUn
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/public'));
 
-app.use('/', express.static(path.join(_dirname, 'static')))
+app.get('/', (req, res)=>{
+    res.render('index')
+})
 
 app.listen(9999, () => {
     console.log('Server live at 9999')
