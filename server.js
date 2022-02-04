4774e1b99fa4d9bb606012438accd9a7ad0279af
msakac <msakac@foi.hr>
2022-02-04 11:46:05 +0100

    Created server.js and added requirements

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const 
+const path = require('path')
+const methodOverride = require('method-override')
 const app = express()
+
+mongoose.connect('mongodb://localhost/tbprojekt', { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.use('/', express.static(path.join(_dirname, 'static')))
+
+app.listen(9999, () => {
+    console.log('Server live at 9999')
+})
