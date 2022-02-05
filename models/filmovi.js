4a3586ba409a9f099acad314f58dafb4421f9736
msakac <msakac@foi.hr>
2022-02-05 14:14:28 +0100

    Kreirana schema za filmove

--- models/filmovi.js
+++ models/filmovi.js
@@ -1,9 +1,35 @@
-const express = require('express')
-const Korisnik = require('./../models/korisnik')
-const router = express.Router()
+const mongoose = require('mongoose')
 
-router.get('/pocetna', (req,res)=>{
-    res.render('pocetna')
+const filmoviSchema = new mongoose.Schema({
+    naziv:{
+        type: String,
+        required: true
+    },
+    opis:{
+        type: String,
+        required: true,
+    },
+    datumIzlaska:{
+        type: Date,
+        default: Date.now
+    },
+    zanr: {
+        type: String,
+    },
+    reziser: {
+        type: String,
+        required: true
+    },
+    glumci: {
+        type: String,
+        required: true,
+    },
+    slika:{
+        type: String,
+    },
+    najava:{
+        type: String,
+    }
 })
 
-module.exports = router
+module.exports = mongoose.model('Filmovi', filmoviSchema)
