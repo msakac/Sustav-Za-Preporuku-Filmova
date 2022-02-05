401a7fe7da1af3b182859c1425d32620e2f4127b
msakac <msakac@foi.hr>
2022-02-05 14:15:19 +0100

    Kreirane get rute za CRUD filmova i dodavanje novog filma. Kreirana post ruta kod dodavanja novog filma

--- routes/filmovi.js
+++ routes/filmovi.js
@@ -1,8 +1,31 @@
 const express = require('express')
+const Film = require('./../models/filmovi')
 const router = express.Router()
 
-router.get('/', (req,res)=>{
-
+//Stranica za CRUD filmova
+router.get('/crud', (req,res)=>{
+    res.render('filmoviManage')
 })
 
+//Stranica za dodavanje filmova
+router.get('/novi',(req,res)=>{
+    res.render('filmoviDodaj', {film: new Film()})
+})
+
+//Dodavanje novog filma
+
+router.post('/novi', async (req, res)=>{
+    const film = new Film({
+        naziv: req.body.naziv,
+        opis: req.body.opis,
+        datumIzlaska: req.body.datumIzlaska,
+        zanr: req.body.zanr,
+        reziser: req.body.reziser,
+        glumci: req.body.glumci,
+        slika: req.body.slika,
+        najava: req.body.najava
+    })
+    await film.save()
+    res.render('filmoviManage')
+})
 module.exports = router
