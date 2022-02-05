42ec3de94f538a06abb32ec2c6e35fcbb50077ee
msakac <msakac@foi.hr>
2022-02-05 10:01:34 +0100

    Dodan model za korisnika. Dodani ruteri za korisnika i filmove. Dodan express paket za sesije. Kod startanja server odnosno stranice, provjeravamo dali sesija postoji. Ukoliko postoji prikazujemo početnu stranicu inače preusmjeravamo na prijavu.

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const path = require('path')
 const methodOverride = require('method-override')
+const Korisnik = require('./models/korisnik')
+const korisnikRouter = require('./routes/korisnik')
+const filmoviRouter = require('./routes/filmovi')
+const session = require('express-session')
 const app = express()
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+  }))
 
 mongoose.connect('mongodb://localhost/tbprojekt', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -11,9 +20,21 @@ app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res)=>{
-    res.render('index')
+app.use('/korisnik', korisnikRouter)
+app.use('/filmovi',filmoviRouter)
+
+app.get('/', async (req, res)=>{
+    console.log(req.session.result)
+    if(req.session.result){
+        res.render('index')
+    }else{
+        res.redirect('/korisnik/login')
+    }
+    
 })
+
+
+
 
 app.listen(9999, () => {
     console.log('Server live at 9999')
