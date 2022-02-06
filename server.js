68da93a49b9b76c03566eb9910dda6fa08b39231
msakac <msakac@foi.hr>
2022-02-06 01:40:47 +0100

    Dodani modeli i rute u server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
+
 const Korisnik = require('./models/korisnik')
 const Filmovi = require('./models/filmovi')
 const Kategorije = require('./models/kategorije')
+const FilmKategorije = require('./models/filmKategorije')
+
 const korisnikRouter = require('./routes/korisnik')
 const filmoviRouter = require('./routes/filmovi')
 const kategorijeRouter = require('./routes/kategorije')
+const filmKategorijeRouter = require('./routes/filmKategorije')
+
 const session = require('express-session')
 const app = express()
 
@@ -26,6 +31,7 @@ app.use(express.static(__dirname + '/public'));
 app.use('/korisnik', korisnikRouter)
 app.use('/filmovi',filmoviRouter)
 app.use('/kategorije', kategorijeRouter)
+app.use('/film-kategorije', filmKategorijeRouter)
 
 app.get('/', async (req, res)=>{
     console.log(req.session.result)
@@ -37,8 +43,6 @@ app.get('/', async (req, res)=>{
     }
     
 })
-
-
 
 
 app.listen(9999, () => {
